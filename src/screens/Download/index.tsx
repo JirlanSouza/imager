7a098412a19c43ba0719d7d3830a4ce460ee8e3c
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-27 20:26:02 -0400

    Changed the layout

--- src/screens/Download/index.tsx
+++ src/screens/Download/index.tsx
@@ -1,22 +1,39 @@
+import { useEffect } from "react";
+
 import { editImageActions } from "../../actions/EditImage";
+import { DownloadScreenHeader } from "../../components/DownloadScreenHeader";
 import { PrimaryButton } from "../../components/PrimaryButton";
+import { PrimaryLinkButton } from "../../components/PrimaryButton/PrimaryLinkButton";
 import { useEditImageStore } from "../../contexts/imageContext";
 import styles from "./styles.module.scss";
 
 export function Download() {
   const { state, dispatch } = useEditImageStore();
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  function clearImagesData() {
+    URL.revokeObjectURL(state.imageSrc);
+    URL.revokeObjectURL(state.imageOut);
+
+    dispatch(editImageActions.setImageSrc(""));
+    dispatch(editImageActions.setImageOut(""));
+  }
+
   return (
-    <main className={styles.app_main}>
-      <PrimaryButton
-        onClick={() => {
-          dispatch(editImageActions.setStep("selectImage"));
-        }}
-      >
-        To home
-      </PrimaryButton>
-      <div className={styles.app_image_out}>
-        <img src={state.imageOut} />
-      </div>
-    </main>
+    <>
+      <DownloadScreenHeader />
+      <main className={styles.download_main}>
+        <div className={styles.app_image_out}>
+          <img src={state.imageOut} />
+        </div>
+
+        <PrimaryLinkButton href={state.imageOut} download={"imager_" + state.imageName}>
+          Salvar imagem
+        </PrimaryLinkButton>
+      </main>
+    </>
   );
 }
