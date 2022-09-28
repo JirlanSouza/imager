323f1bf63ba656fc7afea47444a5c3ea245ad624
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-27 20:25:12 -0400

    Added the return button click handler

--- src/components/EditScreenHeader/index.tsx
+++ src/components/EditScreenHeader/index.tsx
@@ -1,16 +1,25 @@
 import { FaArrowLeft, FaTimes } from "react-icons/fa";
+import { editImageActions } from "../../actions/EditImage";
+import { useEditImageStore } from "../../contexts/imageContext";
 
 import { IconButton } from "../IconButton";
 import { TextButton } from "../TextButton";
 import styles from "./styles.module.scss";
 
 export function EditScreenHeader() {
+  const { dispatch } = useEditImageStore();
+
+  function handleReturn() {
+    dispatch(editImageActions.setStep("selectImage"));
+  }
+
+  function handleReset() {}
+
   return (
     <header className={styles.app_header}>
       <div className={styles.app_header_container}>
-        <IconButton icon={FaArrowLeft} />
+        <IconButton icon={FaArrowLeft} onClick={handleReturn} />
         <TextButton>Reset</TextButton>
-        <IconButton icon={FaTimes} />
       </div>
     </header>
   );
