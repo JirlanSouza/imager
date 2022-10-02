b6ae464fc900d5138516df477438cd36247de5a9
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-01 23:14:26 -0400

    Remove the unused props

--- src/screens/Edit/index.tsx
+++ src/screens/Edit/index.tsx
@@ -1,27 +1,16 @@
-import { useState } from "react";
-
 import { EditScreenHeader } from "../../components/EditScreenHeader";
 import { EditingImage } from "../../components/EditingImage";
 import { EditingComands } from "../../components/EditingComands";
 import styles from "./styles.module.scss";
-import defaultImage from "../../assets/images/default.jpg";
 
 export function Edit() {
-  const [rotateValue, setRotateValue] = useState(0);
-  const [selectedRatio, setSelectedRatio] = useState(0);
-
   return (
     <>
       <EditScreenHeader />
 
       <main className={styles.editing_main}>
-        <EditingImage rotateValue={rotateValue} aspecRatioCode={selectedRatio} imageSrc={defaultImage} />
-        <EditingComands
-          rotateValue={rotateValue}
-          aspectRatioCode={selectedRatio}
-          setRotateValue={setRotateValue}
-          setAspectRatioCode={setSelectedRatio}
-        />
+        <EditingImage />
+        <EditingComands />
       </main>
     </>
   );
